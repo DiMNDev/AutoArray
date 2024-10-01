c03b87bb3d5198c1a4616774df288536978aac37
Joshua Arnold <joshua.arnold@students.snow.edu>
2024-10-01 09:21:48 -0600

    created MyList class

---
+++ AutoArrayLib/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace AutoArrayLib;
+
+public class MyList
+{
+    private string[] data = [1024];
+
+    public void Push(string newData)
+    {
+        for (int i = 0; i < data.length; i++)
+        {
+            if (data[i] == null)
+            {
+                data[i] = newData;
+                break;
+            }
+        }
+    }
+
+    public string Get(int index)
+    {
+        return data[i]?? throw new excecption("Value is null") 
+    }
+}
